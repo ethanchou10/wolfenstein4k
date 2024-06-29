a03f747f901a0041f32733c510a5d0be159a3c97
Ethan Chou <ethanchou10@outlook.com>
2024-06-29 17:18:55 +1000

    picking up a weapon

---
+++ Assets/Scripts/Weapons/HandgunPickup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunPickup : MonoBehaviour
+{
+    public GameObject realHandgun;
+    public GameObject fakeHandgun;
+    public AudioSource handgunPickupSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        realHandgun.SetActive(true);
+        fakeHandgun.SetActive(false);
+        handgunPickupSound.Play();
+    }
+}
