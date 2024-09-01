08f6613933f5ff54ab877d039b01bdf7cfbe6764
Ethan Chou <ethanchou10@outlook.com>
2024-09-01 11:55:39 +1000

    pickup text

--- Assets/Scripts/Weapons/HandgunAmmoPick.cs
+++ Assets/Scripts/Weapons/HandgunAmmoPick.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HandgunAmmoPick : MonoBehaviour
 {
     public GameObject fakeAmmoClip;
     public AudioSource ammoPickupSound;
+    public GameObject pickupDisplay;
 
     void OnTriggerEnter(Collider other)
     {
         fakeAmmoClip.SetActive(false);
-        Destroy(fakeAmmoClip);
         ammoPickupSound.Play();
         GlobalAmmo.handgunAmmo += 10;
+        pickupDisplay.SetActive(false); 
+        pickupDisplay.GetComponent<TextMeshProUGUI>().text = "CLIP OF BULLETS";
+        pickupDisplay.SetActive(true);
     }
 }
--- Assets/Scripts/Weapons/HandgunPickup.cs
+++ Assets/Scripts/Weapons/HandgunPickup.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HandgunPickup : MonoBehaviour
 {
     public GameObject realHandgun;
     public GameObject fakeHandgun;
     public AudioSource handgunPickupSound;
+    public GameObject pickupDisplay;
 
     void OnTriggerEnter(Collider other)
     {
         realHandgun.SetActive(true);
         fakeHandgun.SetActive(false);
-        Destroy(fakeHandgun);
         handgunPickupSound.Play();
+        GetComponent<BoxCollider>().enabled = false;
+        pickupDisplay.SetActive(false); 
+        pickupDisplay.GetComponent<TextMeshProUGUI>().text = "HANDGUN";
+        pickupDisplay.SetActive(true);
     }
 }
