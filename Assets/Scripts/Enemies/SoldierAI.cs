f54526476992c45f106cee5c9d078b19cda41963
Ethan Chou <ethanchou10@outlook.com>
2024-09-29 16:22:00 +1000

    added SoldierAI

---
+++ Assets/Scripts/Enemies/SoldierAI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierAI : MonoBehaviour
+{
+    public string hitTag;
+    public bool lookingAtPlayer = false;
+    public GameObject theSoldier;
+
+    void Update() {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit))
+        {
+            hitTag = Hit.transform.tag;
+        }
+        if (hitTag == "Player")
+        {
+            theSoldier.GetComponent<Animator>().Play("assault_combat_shoot");
+            lookingAtPlayer = true;
+        }
+        else
+        {
+            theSoldier.GetComponent<Animator>().Play("assault_combat_idle");
+            lookingAtPlayer = false;
+        }
+    }
+}
